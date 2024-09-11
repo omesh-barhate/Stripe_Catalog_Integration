ecd36a775776f2a798ac2d90b03e1bbad506e95d
omesh-barhate <barhateomesh@gmail.com>
2024-09-11 12:11:44 +0530

    added url for hook and updated requirements

--- customer_sync/customer_app/urls.py
+++ customer_sync/customer_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomerReadWrite,CustomerReadWriteDelete
+from .views import CustomerReadWrite,CustomerReadWriteDelete,hookdata
 
 urlpatterns=[
     path('customers/',CustomerReadWrite.as_view()),
     path('customers/<str:pk>',CustomerReadWriteDelete.as_view()),
+    path('hook/',hookdata.as_view()),
 ]
